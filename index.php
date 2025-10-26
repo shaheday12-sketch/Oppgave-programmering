429e5df9291bebdc7473a2f50f6c6e338ed9edaa
shaheday12-sketch <shayo1243@usn.no>
2025-10-26 19:13:39 +0100

    endret

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    <meta charset="UTF-8">
+    <title>PRG120V - Vedlikeholdssystem</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Brukerfunksjoner</h1>
+    <p>Velg en handling nedenfor:</p>
+
+    <ul>
+        <li><a href="registrer_klasse.php">Registrer klasse</a></li>
+        <li><a href="vis_klasser.php">Vis alle klasser</a></li>
+        <li><a href="slett_klasse.php">Slett klasse</a></li>
+        <br><br> <!--Her kommer mellomrommet mellom klassene og studentene-->
+        <li><a href="registrer_student.php">Registrer student</a></li>
+        <li><a href="vis_studenter.php">Vis alle studenter</a></li>
+        <li><a href="slett_student.php">Slett student</a></li>
+    </ul>
+</body>
+</html>
